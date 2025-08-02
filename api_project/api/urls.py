09e115308c03323f5712dd24ba76c72317aed54d
Alazar W. Sime <alazarwondwessen47@gmail.com>
2025-08-02 22:26:44 +0300

    i have done with task 2

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -1,6 +1,12 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
 from .views import BookList
+from .views import BookViewSet
+
+router = DefaultRouter()
+router.register(r'books', BookViewSet, basename='book')
 
 urlpatterns = [
-    path('books/', BookList.as_view(), name='book-list'),
+    path('list/', BookList.as_view(), name='book-list'),  # Optional: keep your original endpoint
+    path('', include(router.urls)),  # Includes all ViewSet routes
 ]

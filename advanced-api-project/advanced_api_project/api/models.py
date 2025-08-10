5e970eb04c664eb5499bea568f142f1641888df2
Alazar W. Sime <alazarwondwessen47@gmail.com>
2025-08-10 19:09:24 +0300

    Update models.py

--- advanced-api-project/advanced_api_project/api/models.py
+++ advanced-api-project/advanced_api_project/api/models.py
@@ -1,28 +1,22 @@
 from django.db import models
 
+# Define the Author model to represent authors
 class Author(models.Model):
-    """
-    Author model:
-    - name: authour's full name
-    - Relationship: an Author can have many Books (one-to-many).
-    """
-    name = models.CharField(max_length = 200)
+    name = models.CharField(max_length=200)
 
+    # magic method to have the string representation of the name field
     def __str__(self):
         return self.name
 
+
+# Define the Book model to represent books
 class Book(models.Model):
-    """ 
-    Book model:
-     - title; title of the book.
-     - publication_year: integer year the book was published.
-     - authour: ForeignKey to Author (related_name = 'books').
-     """
-    title = models.CharField(max_length = 255)
+    title = models.CharField(max_length=200)
     publication_year = models.IntegerField()
+    
+    # Foreign key field used to represent 'one-to-many' relationship between the author and books.
     author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name="books")
 
+    # magic method to have the string representation of the title field
     def __str__(self):
-        return f"{self.title} ({self.publication_year})"
-
-# Create your models here.
+        return self.title

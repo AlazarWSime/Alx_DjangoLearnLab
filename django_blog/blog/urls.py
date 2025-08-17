f23a4aaa7837c01b999825f48cd81bf62c5ba886
Alazar W. Sime <alazarwondwessen47@gmail.com>
2025-08-17 21:31:12 +0300

    i have modified urls.py and added profile to it

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -11,23 +11,25 @@ from .views import (
     CommentUpdateView, 
     CommentDeleteView, 
     search_posts,
-    PostByTagListView,  # Import the tag-based view
+    PostByTagListView,
+    ProfileView,  # Import the profile view
 )
 
 urlpatterns = [
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('register/', RegisterView.as_view(), name='register'),
+    path('profile/', ProfileView.as_view(), name='profile'),  # Add profile URL
     path('post/', ListView.as_view(), name='posts'),
-    path('posts/search/', search_posts, name='search_posts'),  # Search posts
+    path('posts/search/', search_posts, name='search_posts'),
     path('post/new/', CreateView.as_view(), name='new-post'),
     path('post/<int:pk>/detail/', DetailView.as_view(), name='post-detail'),
     path('post/<int:pk>/update/', UpdateView.as_view(), name='post-update'),
-    path('post/<int:pk>/delete/', DeleteView.as_view(), name='post-delete'),  # Fixed typo in 'name'
+    path('post/<int:pk>/delete/', DeleteView.as_view(), name='post-delete'),
 
     path('posts/<int:pk>/', BlogDetailView.as_view(), name='post_detail'),  
     path('posts/<int:post_id>/comments/new/', CommentCreateView.as_view(), name='comment_create'), 
     path('comments/<int:pk>/edit/', CommentUpdateView.as_view(), name='comment_update'),  
     path('comments/<int:pk>/delete/', CommentDeleteView.as_view(), name='comment_delete'),
-    path('posts/tags/<slug:tag_slug>/', PostByTagListView.as_view(), name='post_by_tag'),  # Tag-based view
+    path('posts/tags/<slug:tag_slug>/', PostByTagListView.as_view(), name='post_by_tag'),
 ]
